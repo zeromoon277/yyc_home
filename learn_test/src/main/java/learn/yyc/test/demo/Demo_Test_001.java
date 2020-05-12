832d891983ee500a6ff1ea21fd65858b6470ea95
yyc <244920018@qq.com>
2020-05-12 10:34:36 +0800

    hello word

--- learn_test/src/main/java/learn/yyc/test/demo/Demo_Test_001.java
+++ learn_test/src/main/java/learn/yyc/test/demo/Demo_Test_001.java
@@ -6,4 +6,11 @@ package learn.yyc.test.demo;
  * @date 2020/5/12 0012 上午 10:11
  */
 public class Demo_Test_001 {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("my test");
+    }
 }
